7682537e3cc74ce44d8ecca1914d5e78a0f0d47b
Safiya B <safbek@list.ru>
2021-07-21 14:30:54 +0300

    replace the filter with a find

--- src/handlerFullPost.js
+++ src/handlerFullPost.js
@@ -15,19 +15,19 @@ const handlerFullPost = ((state) => {
       e.preventDefault();
       const reviewPost = Number(e.target.dataset.id);
       console.log(reviewPost);
-      const openedPost = stateProxy.updates.posts.flat().filter((post) => post.id === reviewPost);
+      const openedPost = stateProxy.updates.posts.flat().find((post) => post.id === reviewPost);
 
       // to pass data into modal
       const modalTitle = document.querySelector('.modal-title');
       const modalBody = document.querySelector('.modal-body');
-      modalTitle.textContent = openedPost[0].title;
-      modalBody.textContent = openedPost[0].description;
+      modalTitle.textContent = openedPost.title;
+      modalBody.textContent = openedPost.description;
 
       // access to full article
       const fullArticleLink = document.querySelector('.full-article');
-      fullArticleLink.setAttribute('href', openedPost[0].link);
+      fullArticleLink.setAttribute('href', openedPost.link);
 
-      stateProxy.uiState.openPosts.push(openedPost[0].id);
+      stateProxy.uiState.openPosts.push(openedPost.id);
     });
   });
 });
